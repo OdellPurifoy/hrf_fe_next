72c9173b2f669fbd2636ce49be8bdcb916d4f657
Odell Purifoy <opurifoy@springhealth.com>
2023-04-09 18:21:25 -0400

    Added next link and next image

--- src/pages/lounges.js
+++ src/pages/lounges.js
@@ -1,8 +1,12 @@
+import Image from 'next/image'
+import Link from "next/link"
+
 const Lounges = () => {
   return (
     <div>
+      <Link href='/'>Back Home</Link>
       <h1>Here will be a list of all lounges</h1>
-      <img src=''></img>
+      <Image src='/next.svg' alt='logo image' width={200} height={200} />
     </div>
   )
 }
