1ac9c5c41d06e01619538863f6ffa652d95dff8b
Odell Purifoy <opurifoy@springhealth.com>
2023-04-09 18:00:40 -0400

    Finished initial code cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 export default function Home() {
   return (
-    <div>Working</div>
+    <div>
+      <h1>Home</h1>
+      <div>Working now</div>
+    </div>
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     // </main>
   )
